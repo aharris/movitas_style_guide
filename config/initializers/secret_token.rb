80aa53f01dcf16082d65964fc0dafe1eb9b94d2e
Ashton Harris <ahdesignsonline@gmail.com>
2012-04-18 21:36:41 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovitasStyleGuide::Application.config.secret_token = '5f71af223ae92e7320041101d37a3754ff09a52302ec1015392ca29b03c81140a5797f5aa6b296b7d383884669c5da940aed0ba2ee6d7c5d61a791f3ecc6aae9'
